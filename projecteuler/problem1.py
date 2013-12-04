40d47e73dbc841c19ce7571699e9a5cb1c618871
Eyram <arrow@envy-testing5.(none)>
2013-12-04 17:54:59 +0000

    safety addition

--- projecteuler/problem1.py
+++ projecteuler/problem1.py
@@ -1,10 +1,7 @@
 num_range = range(1,1000)
-total = 0
 multiples  = []
 
-for i in num_range:
-    if i % 3 == 0 or i % 5 == 0:
-        multiples.append(i)
+[multiples.append(i) for i in num_range if i % 3 == 0 or i % 5 == 0]
         
 total = sum(multiples)
 
